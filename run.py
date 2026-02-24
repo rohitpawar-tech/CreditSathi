ba918f3c454ffe31917f7dadc46b10207b97c66d
Rohit Pawar <rp035806@gmail.com>
2026-02-24 22:36:21 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import os
+import traceback
 from app import create_app
 
 env = os.environ.get('FLASK_ENV', 'dev')
-app = create_app(env)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+try:
+    app = create_app(env)
+    if __name__ == '__main__':
+        print("🚀 Attempting to start Flask Server...")
+        app.run(host='0.0.0.0', port=5000, debug=True)
+
+except Exception as e:
+    print("\n❌ SERVER CRASHED! Yahan error hai:")
+    print(traceback.format_exc())
